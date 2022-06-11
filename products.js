438fed14b4aef099e4d38081b5c4126e99e2d728
Ellamsg <103334963+Ellamsg@users.noreply.github.com>
2022-06-11 10:01:09 -0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    id: 0,
+    name: "T-shirt 1",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t1.png",
+    
+  },
+  {
+    id: 1,
+    name: "T-shirt 2",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "T-shirt 3",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "T-shirt 4",
+    price: 25.99,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t4.png",
+  },
+ 
+  
+];
+
+
+ 
