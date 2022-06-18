7731e2f1d1ad6d72ec5f00383cf21a718e1053b7
Ellamsg <103334963+Ellamsg@users.noreply.github.com>
2022-06-19 07:35:27 +0800

    Add files via upload

---
+++ cart-main/cart-main/products.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    id: 0,
+    name: "T-shirt 1",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/2022_06_19_05_04_IMG_2505.png",
+    
+  },
+  {
+    id: 1,
+    name: "T-shirt 2",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/2022_06_18_04_52_IMG_2402.png",
+  },
+  {
+    id: 2,
+    name: "T-shirt 3",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/2022_06_19_05_04_IMG_2503.png",
+  },
+  {
+    id: 3,
+    name: "T-shirt 4",
+    price: 25.99,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/2022_06_19_05_04_IMG_2504.png",
+  },
+ 
+  
+];
+
+
+ 
